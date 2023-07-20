358bef298e37999fa744b350beed6289c8fc0b41
Hunter Mellema <hpm@amazon.com>
2023-07-20 09:59:31 -0600

    Add back in multi-source integ test

--- src/it/java/software/amazon/smithy/gradle/MultipleSourcesTest.java
+++ src/it/java/software/amazon/smithy/gradle/MultipleSourcesTest.java
@@ -4,38 +4,36 @@ import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
 import org.junit.jupiter.api.Test;
 
-// TODO: FIX THIS TEST
 public class MultipleSourcesTest {
     @Test
     public void testProjection() {
-//        Utils.withCopy("multiple-sources", buildDir -> {
-//            BuildResult result = GradleRunner.create()
-//                    .forwardOutput()
-//                    .withProjectDir(buildDir)
-//                    .withArguments("clean", "build", "--stacktrace")
-//                    .build();
-//
-//            Utils.assertSmithyBuildRan(result);
-//            Utils.assertValidationRan(result);
-//            Utils.assertArtifactsCreated(buildDir,
-//                    "build/smithyprojections/multiple-sources/source/build-info/smithy-build-info.json",
-//                    "build/smithyprojections/multiple-sources/source/model/model.json",
-//                    "build/smithyprojections/multiple-sources/source/sources/a.smithy",
-//                    "build/smithyprojections/multiple-sources/source/sources/b.smithy",
-//                    "build/smithyprojections/multiple-sources/source/sources/c.smithy",
-//                    "build/smithyprojections/multiple-sources/source/sources/nested/foo.smithy",
-//                    "build/smithyprojections/multiple-sources/source/sources/nested/more/baz.smithy",
-//                    "build/smithyprojections/multiple-sources/source/sources/manifest",
-//                    "build/libs/multiple-sources.jar");
-//            Utils.assertJarContains(buildDir,
-//                    "build/libs/multiple-sources.jar",
-//                    "META-INF/smithy/manifest",
-//                    "META-INF/smithy/a.smithy",
-//                    "META-INF/smithy/b.smithy",
-//                    "META-INF/smithy/c.smithy",
-//                    "META-INF/smithy/nested/foo.smithy",
-//                    "META-INF/smithy/nested/more/baz.smithy");
-//        });
-//    }
+        Utils.withCopy("multiple-sources", buildDir -> {
+            BuildResult result = GradleRunner.create()
+                    .forwardOutput()
+                    .withProjectDir(buildDir)
+                    .withArguments("clean", "build", "--stacktrace")
+                    .build();
+
+            Utils.assertSmithyBuildRan(result);
+            Utils.assertValidationRan(result);
+            Utils.assertArtifactsCreated(buildDir,
+                    "build/smithyprojections/multiple-sources/source/build-info/smithy-build-info.json",
+                    "build/smithyprojections/multiple-sources/source/model/model.json",
+                    "build/smithyprojections/multiple-sources/source/sources/a.smithy",
+                    "build/smithyprojections/multiple-sources/source/sources/b.smithy",
+                    "build/smithyprojections/multiple-sources/source/sources/c.smithy",
+                    "build/smithyprojections/multiple-sources/source/sources/nested/foo.smithy",
+                    "build/smithyprojections/multiple-sources/source/sources/nested/more/baz.smithy",
+                    "build/smithyprojections/multiple-sources/source/sources/manifest",
+                    "build/libs/multiple-sources.jar");
+            Utils.assertJarContains(buildDir,
+                    "build/libs/multiple-sources.jar",
+                    "META-INF/smithy/manifest",
+                    "META-INF/smithy/a.smithy",
+                    "META-INF/smithy/b.smithy",
+                    "META-INF/smithy/c.smithy",
+                    "META-INF/smithy/nested/foo.smithy",
+                    "META-INF/smithy/nested/more/baz.smithy");
+        });
     }
 }

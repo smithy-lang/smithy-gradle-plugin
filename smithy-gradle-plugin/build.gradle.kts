14c06e87dd2df307e15a6eacb327faaeb88f8099
Hunter Mellema <hpm@amazon.com>
2023-08-10 11:10:30 -0600

    Refactor to allow for multiple plugins in the repo

--- smithy-gradle-plugin/build.gradle.kts
+++ smithy-gradle-plugin/build.gradle.kts
@@ -4,6 +4,7 @@ description = "This project integrates Smithy with Gradle. This plugin can build
         "projects, and generate JARs that contain filtered *projections* of Smithy " +
         "models."
 
+<<<<<<< HEAD
 gradlePlugin {
     plugins {
         create("software.amazon.smithy") {
@@ -13,4 +14,10 @@ gradlePlugin {
             implementationClass = "software.amazon.smithy.gradle.SmithyPlugin"
         }
     }
+=======
+ext {
+    set("id","software.amazon.smithy")
+    set("displayName", "Smithy Gradle Plugin")
+    set("implementationClass", "software.amazon.smithy.gradle.SmithyPlugin")
+>>>>>>> 5573798 (Refactor to allow for multiple plugins in the repo)
 }

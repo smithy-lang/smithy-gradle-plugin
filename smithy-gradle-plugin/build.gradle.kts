7f11eeadcf8865dbd6665b1c121166ceb636d9ef
Hunter Mellema <hpm@amazon.com>
2023-08-10 14:22:16 -0600

    Update to fix publish error

--- smithy-gradle-plugin/build.gradle.kts
+++ smithy-gradle-plugin/build.gradle.kts
@@ -4,7 +4,6 @@ description = "This project integrates Smithy with Gradle. This plugin can build
         "projects, and generate JARs that contain filtered *projections* of Smithy " +
         "models."
 
-<<<<<<< HEAD
 gradlePlugin {
     plugins {
         create("software.amazon.smithy") {
@@ -14,10 +13,4 @@ gradlePlugin {
             implementationClass = "software.amazon.smithy.gradle.SmithyPlugin"
         }
     }
-=======
-ext {
-    set("id","software.amazon.smithy")
-    set("displayName", "Smithy Gradle Plugin")
-    set("implementationClass", "software.amazon.smithy.gradle.SmithyPlugin")
->>>>>>> 5573798 (Refactor to allow for multiple plugins in the repo)
 }

3083bc5ec4aea55df86e4c05a223915a84d423a1
Hunter Mellema <124718352+hpmellema@users.noreply.github.com>
2023-12-21 06:22:05 -0700

    Update to use InputFiles for build config parameter (#111)

--- smithy-base-plugin/src/main/java/software/amazon/smithy/gradle/tasks/SmithyBuildTask.java
+++ smithy-base-plugin/src/main/java/software/amazon/smithy/gradle/tasks/SmithyBuildTask.java
@@ -16,6 +16,7 @@ import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.Property;
 import org.gradle.api.provider.SetProperty;
 import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.OutputDirectory;
@@ -61,7 +62,7 @@ public abstract class SmithyBuildTask extends AbstractSmithyCliTask {
      *
      * @return list of smithy-build config json files
      */
-    @Input
+    @InputFiles
     public abstract Property<FileCollection> getSmithyBuildConfigs();
 
 
